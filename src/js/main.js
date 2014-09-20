230d71ba7a734bd5ae7af8c0d338effafe11acda
Mark Biesheuvel <mail@markbiesheuvel.nl>
2014-09-20 22:33:47 +0200

    Use Github API

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,23 @@
 $(function () {
 
-    $('p').widowFix();
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.github.com/repos/MarkBiesheuvel/markbiesheuvel.nl/commits?page=1&per_page=10',
+        headers: {
+            'Accept': 'application/vnd.github.v3+json'
+        },
+        success: function(commits){
 
+            var html = '<ul>';
+
+            $.each(commits, function(i, commit){
+                html += '<li>'+commit.commit.message+'</li>';
+            });
+
+            html += '</ul>';
+
+            $('#panel-github').html(html);
+        }
+    });
 });
 
