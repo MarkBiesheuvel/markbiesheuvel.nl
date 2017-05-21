66f4dc53a2544f1a0f3a177187dfa1fd3e78ec1b
Mark Biesheuvel <mail@markbiesheuvel.nl>
2017-05-21 12:10:29 +0200

    Format terraform file

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,10 +41,10 @@ module "dns" {
 }
 
 module "codepipeline" {
-  source          = "codepipeline"
-  url             = "${var.url}"
-  name            = "${replace("${var.url}", ".", "-")}" # Dot is not allowed in some resource names
-  website_s3_name = "${module.website.s3_name}"
-  website_s3_arn  = "${module.website.s3_arn}"
-  website_cloudfront_arn  = "${module.website.cloudfront_arn}"
+  source                 = "codepipeline"
+  url                    = "${var.url}"
+  name                   = "${replace("${var.url}", ".", "-")}" # Dot is not allowed in some resource names
+  website_s3_name        = "${module.website.s3_name}"
+  website_s3_arn         = "${module.website.s3_arn}"
+  website_cloudfront_arn = "${module.website.cloudfront_arn}"
 }
